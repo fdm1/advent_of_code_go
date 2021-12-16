318acf0a9bbe0e33a4082b0e69a054f76f50e358
Frank Massi <frankmassi@gmail.com>
2021-12-16 08:49:28 -0600

    scaffold 2021-16

---
+++ pkg/advent_of_code/2021/16/day16.go
@@ -0,0 +1,19 @@
+package y2021d16
+
+import (
+  "fmt"
+  "github.com/fdm1/advent_of_code_go/pkg/aoc_utils"
+)
+
+
+func Part1(cache bool) string {
+  content := aoc_utils.DownloadInput(2021, 16, cache)
+  input := aoc_utils.InputToSlice(content)
+  return fmt.Sprintf("%v", input)
+}
+
+func Part2(cache bool) string {
+  content := aoc_utils.DownloadInput(2021, 16, cache)
+  input := aoc_utils.InputToSlice(content)
+  return fmt.Sprintf("%v", input)
+}
