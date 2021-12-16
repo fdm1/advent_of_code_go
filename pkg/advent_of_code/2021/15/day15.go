0ef4c4fba459b75cec737fb6932b4f8dfe5cf7ff
Frank Massi <frankmassi@gmail.com>
2021-12-15 19:56:03 -0600

    2015-15 part 2 (sorta)
    Tests pass, but the actual answer doesn't pass on AoC. :(

--- pkg/advent_of_code/2021/15/day15.go
+++ pkg/advent_of_code/2021/15/day15.go
@@ -5,7 +5,6 @@ import (
   "github.com/fdm1/advent_of_code_go/pkg/aoc_utils"
 )
 
-
 func Part1(cache bool) string {
   content := aoc_utils.DownloadInput(2021, 15, cache)
   grid := aoc_utils.InputToSliceOfIntSlices(content)
@@ -30,8 +29,36 @@ func Part1(cache bool) string {
 
 func Part2(cache bool) string {
   content := aoc_utils.DownloadInput(2021, 15, cache)
-  input := aoc_utils.InputToSlice(content)
-  return fmt.Sprintf("%v", input)
+  grid := aoc_utils.InputToSliceOfIntSlices(content)
+  initialRows := len(grid)
+  initialColumns := len(grid[0])
+  grid = ExpandGrid(grid, initialRows, initialColumns)
+  grid = ExpandGrid(grid, initialRows, initialColumns)
+  grid = ExpandGrid(grid, initialRows, initialColumns)
+  grid = ExpandGrid(grid, initialRows, initialColumns)
+
+  maxRisk := InitialRisk(grid)
+  endpointRisks := map[string]int{}
+  endpointRisks["0,1"] = grid[0][1]
+  endpointRisks["1,0"] = grid[1][0]
+
+  running := true
+  risk := 0
+
+  for running {
+    endpointRisks = Move(endpointRisks, grid, maxRisk)
+    if len(endpointRisks) == 1 {
+      for point, riskVal := range endpointRisks {
+        x, y := aoc_utils.GridPointToInts(point)
+        if x == len(grid[0]) - 1 && y == len(grid) - 1 {
+          risk = riskVal
+          running = false
+        }
+      }
+    }
+  }
+
+  return fmt.Sprintf("%v", risk)
 }
 
 // Get a value as a baseline
@@ -54,7 +81,7 @@ func Move(pathEndpoints map[string]int, grid [][]int, maxRisk int) map[string]in
     if x < len(grid[0]) - 1 {
       newPoint := aoc_utils.GridIntsToPoint(x+1, y)
       newRisk := currentRisk + grid[x+1][y]
-      if newEndpoints[newPoint] == 0 && newRisk < maxRisk {
+      if newEndpoints[newPoint] == 0 && newRisk <= maxRisk {
         newEndpoints[newPoint] = newRisk
       } else if newEndpoints[newPoint] > 0 && newRisk < newEndpoints[newPoint] {
         newEndpoints[newPoint] = newRisk
@@ -63,7 +90,7 @@ func Move(pathEndpoints map[string]int, grid [][]int, maxRisk int) map[string]in
     if y < len(grid) - 1 {
       newPoint := aoc_utils.GridIntsToPoint(x, y+1)
       newRisk := currentRisk + grid[x][y+1]
-      if newEndpoints[newPoint] == 0 && newRisk < maxRisk {
+      if newEndpoints[newPoint] == 0 && newRisk <= maxRisk {
         newEndpoints[newPoint] = newRisk
       } else if newEndpoints[newPoint] > 0 && newRisk < newEndpoints[newPoint] {
         newEndpoints[newPoint] = newRisk
@@ -71,4 +98,34 @@ func Move(pathEndpoints map[string]int, grid [][]int, maxRisk int) map[string]in
     }
   }
   return newEndpoints
+}
+
+func ExpandGrid(grid [][]int, rows int, columns int) [][]int {
+  newGrid := [][]int{}
+  for row, rowVals := range grid {
+    newRow := grid[row]
+    for c := 0; c < columns; c++ {
+      oldVal := rowVals[len(rowVals) - (columns - c)]
+      if oldVal == 9 {
+        newRow = append(newRow, 1)
+      } else {
+        newRow = append(newRow, oldVal + 1)
+      }
+    }
+    newGrid = append(newGrid, newRow)
+  }
+  for r := 0; r < rows; r++ {
+    newRow := []int{}
+    for _, val := range newGrid[len(newGrid) - rows] { newRow = append(newRow, val) }
+    for i, val := range newRow {
+      if val == 9 {
+        newRow[i] = 1
+      } else {
+        newRow[i] = val + 1
+      }
+    }
+    newGrid = append(newGrid, newRow)
+  }
+
+  return newGrid
 }
