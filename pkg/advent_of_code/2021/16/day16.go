a0ce3dc265ea0b7a66038974871261ee35fe51a2
Frank Massi <frankmassi@gmail.com>
2021-12-20 09:01:45 -0600

    WIP

--- pkg/advent_of_code/2021/16/day16.go
+++ pkg/advent_of_code/2021/16/day16.go
@@ -2,6 +2,8 @@ package y2021d16
 
 import (
   "fmt"
+  "strconv"
+  "strings"
   "github.com/fdm1/advent_of_code_go/pkg/aoc_utils"
 )
 
@@ -9,11 +11,84 @@ import (
 func Part1(cache bool) string {
   content := aoc_utils.DownloadInput(2021, 16, cache)
   input := aoc_utils.InputToSlice(content)
-  return fmt.Sprintf("%v", input)
+  return fmt.Sprintf("%v", CalculateSum(input[0], 0, []int64{}, []int64{}))
 }
 
 func Part2(cache bool) string {
   content := aoc_utils.DownloadInput(2021, 16, cache)
   input := aoc_utils.InputToSlice(content)
   return fmt.Sprintf("%v", input)
+}
+
+func BinaryToInt(binString string) int64 {
+  output, _ := strconv.ParseInt(binString, 2, 64)
+  return output
+}
+
+// Probably a "right" way to do this, but I hate this stuff
+func ConvertToBinary(hexString string) []string {
+  chars := strings.Split(hexString, "")
+  result := ""
+  for _, char := range chars {
+    if char == "0" { result += "0000" }
+    if char == "1" { result += "0001" }
+    if char == "2" { result += "0010" }
+    if char == "3" { result += "0011" }
+    if char == "4" { result += "0100" }
+    if char == "5" { result += "0101" }
+    if char == "6" { result += "0110" }
+    if char == "7" { result += "0111" }
+    if char == "8" { result += "1000" }
+    if char == "A" { result += "1001" }
+    if char == "B" { result += "1010" }
+    if char == "C" { result += "1011" }
+    if char == "D" { result += "1101" }
+    if char == "E" { result += "1110" }
+    if char == "F" { result += "1111" }
+    fmt.Println(result)
+  }
+  return strings.Split(result, "")
+}
+
+// return versions and values
+func CalculateSum(hexString string, pointer int, versions []int64, values []int64) int64 {
+  bits := ConvertToBinary(hexString)
+  fmt.Printf("starting over @ pointer = %v; len(bits)=%v\n", pointer, len(bits))
+  if pointer < len(bits) - 1 {
+    fmt.Println(strings.Join(bits, ""))
+    newVersions := versions
+    newValues := values
+
+    versionString := strings.Join(bits[pointer:pointer+3], "")
+    currentVersion := BinaryToInt(versionString)
+    newVersions = append(newVersions, currentVersion)
+    pointer += 3
+    fmt.Printf("pointer = %v; version = %v (%v)\n", pointer, currentVersion, versionString)
+
+    typeString := strings.Join(bits[pointer:pointer+3], "")
+    typeId := BinaryToInt(typeString)
+    pointer += 3
+    fmt.Printf("pointer = %v; typeId = %v (%v)\n", pointer, typeId, typeString)
+    if typeId == 4 {
+      for i := 0; i < 3; i++ {
+        pointer += 1
+        newValues = append(newValues, BinaryToInt(strings.Join(bits[pointer:pointer+4], "")))
+        pointer += 4
+        fmt.Printf("pointer = %v\n", pointer)
+      }
+      for pointer % 4 != 0 {
+        pointer += 1
+      }
+      return CalculateSum(hexString, pointer, newVersions, newValues)
+    }
+  } else {
+    fmt.Println(versions)
+    fmt.Println(values)
+    var result int64
+    for _, version := range versions {
+      result += version
+    }
+    return result
+  }
+  return 0
 }
--- pkg/advent_of_code/2021/16/day16_test.go
+++ pkg/advent_of_code/2021/16/day16_test.go
@@ -1,19 +1,21 @@
 package y2021d16
 
 import (
+  "fmt"
   "testing"
-  "github.com/fdm1/advent_of_code_go/pkg/aoc_utils"
+  "strings"
+  // "github.com/fdm1/advent_of_code_go/pkg/aoc_utils"
 )
 
-func TestPart1(t *testing.T) {
-  aoc_utils.MockAdventOfCodeInput(2021, 16)
-  result := Part1(false)
-  expectedResult := "31"
-
-  if result != expectedResult {
-    t.Fatalf("actual result %v != expected result %v", result, expectedResult)
-  }
-}
+// func TestPart1(t *testing.T) {
+//   aoc_utils.MockAdventOfCodeInput(2021, 16)
+//   result := Part1(false)
+//   expectedResult := "31"
+//
+//   if result != expectedResult {
+//     t.Fatalf("actual result %v != expected result %v", result, expectedResult)
+//   }
+// }
 
 // func TestPart2(t *testing.T) {
 //   aoc_utils.MockAdventOfCodeInput(2021, 16)
@@ -24,3 +26,47 @@ func TestPart1(t *testing.T) {
 //     t.Fatalf("actual result %v != expected result %v", result, expectedResult)
 //   }
 // }
+
+func TestConvertToBinary(t *testing.T) {
+  result := ConvertToBinary("D2FE28")
+  expectedResult := strings.Split("110100101111111000101000", "")
+  if len(result) != len(expectedResult) {
+    t.Fatalf("actual result %v != expected result %v", result, expectedResult)
+  }
+  for i, _ := range result {
+    if result[i] != expectedResult[i] {
+      t.Fatalf("actual result %v != expected result %v", result, expectedResult)
+    }
+  }
+}
+
+func TestCalculateSum(t *testing.T) {
+  hexStrings := []string{
+    "D2FE28",
+    "D2FE28D2FE28",
+    // "8A004A801A8002F478",
+    // "620080001611562C8802118E34",
+    // "C0015000016115A2E0802F182340",
+    // "A0016C880162017C3686B18A3D4780",
+  }
+  sums := []int64{
+    6,
+    12,
+    16,
+    12,
+    23,
+    31,
+  }
+  failed := false
+  for i, hexString := range hexStrings {
+    result := CalculateSum(hexString, 0, []int64{}, []int64{})
+    expectedResult := sums[i]
+
+    if result != expectedResult {
+      failed = true
+      fmt.Printf("actual result %v != expected result %v (hexString = %v)\n", result, expectedResult, hexString)
+    }
+  }
+
+  if failed { t.Fatalf("Test failed") }
+}
